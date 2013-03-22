1be5853fd48cfb20552829412a1b78765e2ddc8e
Mark Vasilkov <mvasilkov@gmail.com>
2013-03-22 18:31:11 +0200

    Remove window.requestAnimFrame
    And nothing of value was lost.

--- src/pixi/utils/Utils.js
+++ src/pixi/utils/Utils.js
@@ -1,17 +1,3 @@
-/**
- * Provides requestAnimationFrame in a cross browser way.
- */
-window.requestAnimFrame = (function() {
-  return window.requestAnimationFrame ||
-         window.webkitRequestAnimationFrame ||
-         window.mozRequestAnimationFrame ||
-         window.oRequestAnimationFrame ||
-         window.msRequestAnimationFrame ||
-         function(/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
-           window.setTimeout(callback, 1000/60);
-         };
-})();
-
 function HEXtoRGB(hex) {
 	return [(hex >> 16 & 0xFF) / 255, ( hex >> 8 & 0xFF) / 255, (hex & 0xFF)/ 255];
 }
